4fcf990835ba3abe935881e87540d8b16fe1d66b
zhzluke96 <zhz961101@gmail.com>
2023-04-18 01:19:35 +0800

    :bug: style fix

--- src/builtin/Style.ts
+++ src/builtin/Style.ts
@@ -12,6 +12,18 @@ type StyleComponent = FC<
   [StyleFn | NestedCSSProperties | void]
 >;
 
+const zipStyleFn = (
+  styleOrFn?: StyleFn | NestedCSSProperties | null | void
+): StyleFn => {
+  if (!styleOrFn) {
+    return () => ({ '--required-style-function': '1' });
+  }
+  if (typeof styleOrFn === 'function') {
+    return styleOrFn;
+  }
+  return () => styleOrFn;
+};
+
 /**
  * Adaptive nested css style definition components
  */
@@ -19,14 +31,7 @@ export const Style: StyleComponent = (
   { styleFn, style, ...props },
   styleOrFunc
 ) => {
-  const _styleFn =
-    styleFn || style
-      ? () => style!
-      : false || styleOrFunc
-      ? typeof styleOrFunc === 'function'
-        ? styleOrFunc
-        : () => styleOrFunc
-      : false || (() => ({ '--required-style-function': '1' }));
+  const _styleFn = zipStyleFn(styleFn || style || styleOrFunc);
   const { className, dom } = rStyle(_styleFn);
   // WARN 这个事件其实已经废弃了，但是...还是可以用，每个浏览器基本都实现了
   // ref: https://caniuse.com/mutation-events
@@ -56,14 +61,7 @@ export const GlobalStyle: StyleComponent = (
   { styleFn, style, ...props },
   styleOrFunc
 ) => {
-  const _styleFn =
-    styleFn || style
-      ? () => style!
-      : false || styleOrFunc
-      ? typeof styleOrFunc === 'function'
-        ? styleOrFunc
-        : () => styleOrFunc
-      : false || (() => ({ '--required-style-function': '1' }));
+  const _styleFn = zipStyleFn(styleFn || style || styleOrFunc);
   const { className, dom } = rStyle(_styleFn);
   document.head.parentElement?.classList.add(className);
   document.head.appendChild(dom);
