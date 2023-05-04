06566cd1f929f7f246b4f5cb89959806d3127acd
zhzluke96 <zhz961101@gmail.com>
2023-05-05 00:15:30 +0800

    :sparkles: lazy component

---
+++ src/tools/lazy.ts
@@ -0,0 +1,23 @@
+import { ref } from '@vue/reactivity';
+import { FunctionComponent, SetupComponent, rh } from '../rh';
+
+type ModuleLike<T> = { default: T };
+type RhComponent = FunctionComponent | SetupComponent;
+export const lazy = <Component extends RhComponent>(
+  module_loader: () => Promise<ModuleLike<RhComponent>>
+) => {
+  let module: ModuleLike<RhComponent> | null = null;
+  let p: Promise<ModuleLike<RhComponent>> | null = null;
+
+  const ensure_module = () =>
+    p || (p = module_loader().then((result) => (module = result)));
+
+  return ((props: any, ...children: any[]) => {
+    const moduleRef = ref(module);
+    if (!module) {
+      ensure_module().then((module) => (moduleRef.value = module));
+    }
+    return () =>
+      moduleRef.value ? rh(moduleRef.value.default, props, ...children) : null;
+  }) as unknown as Component;
+};
