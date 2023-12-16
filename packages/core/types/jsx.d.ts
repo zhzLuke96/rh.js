feb8ea127986daee8f23d207c7c11ef31ef28585
zhzluke96 <zhz961101@gmail.com>
2023-12-16 18:17:36 +0800

    :fix: fix jsx type

--- packages/core/types/jsx.d.ts
+++ packages/core/types/jsx.d.ts
@@ -1,5 +1,9 @@
 type Booleanish = boolean | 'true' | 'false';
 
+type Ref<T> = { value: T };
+type Getter<T> = (...args: any[]) => T;
+type MaybeRefLike<T> = T | Ref<T> | Getter<T>;
+
 declare namespace JSX {
   export interface IntrinsicAttributes {
     key?: any;
@@ -14,11 +18,7 @@ declare namespace JSX {
     children?: any;
   }
 
-  export interface RefLike<T> {
-    value: T;
-  }
-
-  export type Refer<T> = T | RefLike<T>;
+  export type RefLike<T> = MaybeRefLike<T>;
 
   export type UnRef<T> = T extends RefLike<infer V> ? V : T;
 
@@ -1161,71 +1161,71 @@ declare namespace JSX {
   // All the WAI-ARIA 1.1 attributes from https://www.w3.org/TR/wai-aria-1.1/
   export interface AriaAttributes {
     /** Identifies the currently active element when DOM focus is on a composite widget, textbox, group, or application. */
-    'aria-activedescendant'?: Refer<string | undefined>;
+    'aria-activedescendant'?: RefLike<string | undefined>;
     /** Indicates whether assistive technologies will present all, or only parts of, the changed region based on the change notifications defined by the aria-relevant attribute. */
-    'aria-atomic'?: Refer<Booleanish | undefined>;
+    'aria-atomic'?: RefLike<Booleanish | undefined>;
     /**
      * Indicates whether inputting text could trigger display of one or more predictions of the user's intended value for an input and specifies how predictions would be
      * presented if they are made.
      */
-    'aria-autocomplete'?: Refer<
+    'aria-autocomplete'?: RefLike<
       'none' | 'inline' | 'list' | 'both' | undefined
     >;
     /** Indicates an element is being modified and that assistive technologies MAY want to wait until the modifications are complete before exposing them to the user. */
-    'aria-busy'?: Refer<Booleanish | undefined>;
+    'aria-busy'?: RefLike<Booleanish | undefined>;
     /**
      * Indicates the current "checked" state of checkboxes, radio buttons, and other widgets.
      * @see aria-pressed
      * @see aria-selected.
      */
-    'aria-checked'?: Refer<Booleanish | 'mixed' | undefined>;
+    'aria-checked'?: RefLike<Booleanish | 'mixed' | undefined>;
     /**
      * Defines the total number of columns in a table, grid, or treegrid.
      * @see aria-colindex.
      */
-    'aria-colcount'?: Refer<number | undefined>;
+    'aria-colcount'?: RefLike<number | undefined>;
     /**
      * Defines an element's column index or position with respect to the total number of columns within a table, grid, or treegrid.
      * @see aria-colcount
      * @see aria-colspan.
      */
-    'aria-colindex'?: Refer<number | undefined>;
+    'aria-colindex'?: RefLike<number | undefined>;
     /**
      * Defines the number of columns spanned by a cell or gridcell within a table, grid, or treegrid.
      * @see aria-colindex
      * @see aria-rowspan.
      */
-    'aria-colspan'?: Refer<number | undefined>;
+    'aria-colspan'?: RefLike<number | undefined>;
     /**
      * Identifies the element (or elements) whose contents or presence are controlled by the current element.
      * @see aria-owns.
      */
-    'aria-controls'?: Refer<string | undefined>;
+    'aria-controls'?: RefLike<string | undefined>;
     /** Indicates the element that represents the current item within a container or set of related elements. */
-    'aria-current'?: Refer<
+    'aria-current'?: RefLike<
       Booleanish | 'page' | 'step' | 'location' | 'date' | 'time' | undefined
     >;
     /**
      * Identifies the element (or elements) that describes the object.
      * @see aria-labelledby
      */
-    'aria-describedby'?: Refer<string | undefined>;
+    'aria-describedby'?: RefLike<string | undefined>;
     /**
      * Identifies the element that provides a detailed, extended description for the object.
      * @see aria-describedby.
      */
-    'aria-details'?: Refer<string | undefined>;
+    'aria-details'?: RefLike<string | undefined>;
     /**
      * Indicates that the element is perceivable but disabled, so it is not editable or otherwise operable.
      * @see aria-hidden
      * @see aria-readonly.
      */
-    'aria-disabled'?: Refer<Booleanish | undefined>;
+    'aria-disabled'?: RefLike<Booleanish | undefined>;
     /**
      * Indicates what functions can be performed when a dragged object is released on the drop target.
      * @deprecated in ARIA 1.1
      */
-    'aria-dropeffect'?: Refer<
+    'aria-dropeffect'?: RefLike<
       'none' | 'copy' | 'execute' | 'link' | 'move' | 'popup' | undefined
     >;
     /**
@@ -1233,89 +1233,89 @@ declare namespace JSX {
      * @see aria-invalid
      * @see aria-describedby.
      */
-    'aria-errormessage'?: Refer<string | undefined>;
+    'aria-errormessage'?: RefLike<string | undefined>;
     /** Indicates whether the element, or another grouping element it controls, is currently expanded or collapsed. */
-    'aria-expanded'?: Refer<Booleanish | undefined>;
+    'aria-expanded'?: RefLike<Booleanish | undefined>;
     /**
      * Identifies the next element (or elements) in an alternate reading order of content which, at the user's discretion,
      * allows assistive technology to override the general default of reading in document source order.
      */
-    'aria-flowto'?: Refer<string | undefined>;
+    'aria-flowto'?: RefLike<string | undefined>;
     /**
      * Indicates an element's "grabbed" state in a drag-and-drop operation.
      * @deprecated in ARIA 1.1
      */
-    'aria-grabbed'?: Refer<Booleanish | undefined>;
+    'aria-grabbed'?: RefLike<Booleanish | undefined>;
     /** Indicates the availability and type of interactive popup element, such as menu or dialog, that can be triggered by an element. */
-    'aria-haspopup'?: Refer<
+    'aria-haspopup'?: RefLike<
       Booleanish | 'menu' | 'listbox' | 'tree' | 'grid' | 'dialog' | undefined
     >;
     /**
      * Indicates whether the element is exposed to an accessibility API.
      * @see aria-disabled.
      */
-    'aria-hidden'?: Refer<Booleanish | undefined>;
+    'aria-hidden'?: RefLike<Booleanish | undefined>;
     /**
      * Indicates the entered value does not conform to the format expected by the application.
      * @see aria-errormessage.
      */
-    'aria-invalid'?: Refer<Booleanish | 'grammar' | 'spelling' | undefined>;
+    'aria-invalid'?: RefLike<Booleanish | 'grammar' | 'spelling' | undefined>;
     /** Indicates keyboard shortcuts that an author has implemented to activate or give focus to an element. */
-    'aria-keyshortcuts'?: Refer<string | undefined>;
+    'aria-keyshortcuts'?: RefLike<string | undefined>;
     /**
      * Defines a string value that labels the current element.
      * @see aria-labelledby.
      */
-    'aria-label'?: Refer<string | undefined>;
+    'aria-label'?: RefLike<string | undefined>;
     /**
      * Identifies the element (or elements) that labels the current element.
      * @see aria-describedby.
      */
-    'aria-labelledby'?: Refer<string | undefined>;
+    'aria-labelledby'?: RefLike<string | undefined>;
     /** Defines the hierarchical level of an element within a structure. */
-    'aria-level'?: Refer<number | undefined>;
+    'aria-level'?: RefLike<number | undefined>;
     /** Indicates that an element will be updated, and describes the types of updates the user agents, assistive technologies, and user can expect from the live region. */
-    'aria-live'?: Refer<'off' | 'assertive' | 'polite' | undefined>;
+    'aria-live'?: RefLike<'off' | 'assertive' | 'polite' | undefined>;
     /** Indicates whether an element is modal when displayed. */
-    'aria-modal'?: Refer<Booleanish | undefined>;
+    'aria-modal'?: RefLike<Booleanish | undefined>;
     /** Indicates whether a text box accepts multiple lines of input or only a single line. */
-    'aria-multiline'?: Refer<Booleanish | undefined>;
+    'aria-multiline'?: RefLike<Booleanish | undefined>;
     /** Indicates that the user may select more than one item from the current selectable descendants. */
-    'aria-multiselectable'?: Refer<Booleanish | undefined>;
+    'aria-multiselectable'?: RefLike<Booleanish | undefined>;
     /** Indicates whether the element's orientation is horizontal, vertical, or unknown/ambiguous. */
-    'aria-orientation'?: Refer<'horizontal' | 'vertical' | undefined>;
+    'aria-orientation'?: RefLike<'horizontal' | 'vertical' | undefined>;
     /**
      * Identifies an element (or elements) in order to define a visual, functional, or contextual parent/child relationship
      * between DOM elements where the DOM hierarchy cannot be used to represent the relationship.
      * @see aria-controls.
      */
-    'aria-owns'?: Refer<string | undefined>;
+    'aria-owns'?: RefLike<string | undefined>;
     /**
      * Defines a short hint (a word or short phrase) intended to aid the user with data entry when the control has no value.
      * A hint could be a sample value or a brief description of the expected format.
      */
-    'aria-placeholder'?: Refer<string | undefined>;
+    'aria-placeholder'?: RefLike<string | undefined>;
     /**
      * Defines an element's number or position in the current set of listitems or treeitems. Not required if all elements in the set are present in the DOM.
      * @see aria-setsize.
      */
-    'aria-posinset'?: Refer<number | undefined>;
+    'aria-posinset'?: RefLike<number | undefined>;
     /**
      * Indicates the current "pressed" state of toggle buttons.
      * @see aria-checked
      * @see aria-selected.
      */
-    'aria-pressed'?: Refer<Booleanish | 'mixed' | undefined>;
+    'aria-pressed'?: RefLike<Booleanish | 'mixed' | undefined>;
     /**
      * Indicates that the element is not editable, but is otherwise operable.
      * @see aria-disabled.
      */
-    'aria-readonly'?: Refer<Booleanish | undefined>;
+    'aria-readonly'?: RefLike<Booleanish | undefined>;
     /**
      * Indicates what notifications the user agent will trigger when the accessibility tree within a live region is modified.
      * @see aria-atomic.
      */
-    'aria-relevant'?: Refer<
+    'aria-relevant'?: RefLike<
       | 'additions'
       | 'additions removals'
       | 'additions text'
@@ -1329,52 +1329,52 @@ declare namespace JSX {
       | undefined
     >;
     /** Indicates that user input is required on the element before a form may be submitted. */
-    'aria-required'?: Refer<Booleanish | undefined>;
+    'aria-required'?: RefLike<Booleanish | undefined>;
     /** Defines a human-readable, author-localized description for the role of an element. */
-    'aria-roledescription'?: Refer<string | undefined>;
+    'aria-roledescription'?: RefLike<string | undefined>;
     /**
      * Defines the total number of rows in a table, grid, or treegrid.
      * @see aria-rowindex.
      */
-    'aria-rowcount'?: Refer<number | undefined>;
+    'aria-rowcount'?: RefLike<number | undefined>;
     /**
      * Defines an element's row index or position with respect to the total number of rows within a table, grid, or treegrid.
      * @see aria-rowcount
      * @see aria-rowspan.
      */
-    'aria-rowindex'?: Refer<number | undefined>;
+    'aria-rowindex'?: RefLike<number | undefined>;
     /**
      * Defines the number of rows spanned by a cell or gridcell within a table, grid, or treegrid.
      * @see aria-rowindex
      * @see aria-colspan.
      */
-    'aria-rowspan'?: Refer<number | undefined>;
+    'aria-rowspan'?: RefLike<number | undefined>;
     /**
      * Indicates the current "selected" state of various widgets.
      * @see aria-checked
      * @see aria-pressed.
      */
-    'aria-selected'?: Refer<Booleanish | undefined>;
+    'aria-selected'?: RefLike<Booleanish | undefined>;
     /**
      * Defines the number of items in the current set of listitems or treeitems. Not required if all elements in the set are present in the DOM.
      * @see aria-posinset.
      */
-    'aria-setsize'?: Refer<number | undefined>;
+    'aria-setsize'?: RefLike<number | undefined>;
     /** Indicates if items in a table or grid are sorted in ascending or descending order. */
-    'aria-sort'?: Refer<
+    'aria-sort'?: RefLike<
       'none' | 'ascending' | 'descending' | 'other' | undefined
     >;
     /** Defines the maximum allowed value for a range widget. */
-    'aria-valuemax'?: Refer<number | undefined>;
+    'aria-valuemax'?: RefLike<number | undefined>;
     /** Defines the minimum allowed value for a range widget. */
-    'aria-valuemin'?: Refer<number | undefined>;
+    'aria-valuemin'?: RefLike<number | undefined>;
     /**
      * Defines the current value for a range widget.
      * @see aria-valuetext.
      */
-    'aria-valuenow'?: Refer<number | undefined>;
+    'aria-valuenow'?: RefLike<number | undefined>;
     /** Defines the human readable text alternative of aria-valuenow for a range widget. */
-    'aria-valuetext'?: Refer<string | undefined>;
+    'aria-valuetext'?: RefLike<string | undefined>;
   }
 
   // All the WAI-ARIA 1.1 role attribute values from https://www.w3.org/TR/wai-aria-1.1/#role_definitions
