bed96dc4884d43190deed5a717770b943eda41dc
zhzluke96 <zhz961101@gmail.com>
2023-12-16 22:09:01 +0800

    :fix: fix unrefx

--- packages/core/src/reactivity.ts
+++ packages/core/src/reactivity.ts
@@ -3,6 +3,7 @@ import {
   resetTracking,
   enableTracking,
   MaybeRefOrGetter,
+  unref,
 } from '@vue/reactivity';
 
 export function skip<T, ARGS extends any[]>(
@@ -29,7 +30,9 @@ export function unskip<T, ARGS extends any[]>(
 }
 
 export const unrefx = <T>(target: T): T extends { value: infer P } ? P : T =>
-  (target as any)?.value;
+  target && typeof target === 'object' && 'value' in target
+    ? target?.value
+    : (target as any);
 
 export const untrack = <T>(value: MaybeRefOrGetter<T>): T =>
   typeof value === 'function' ? skip(value as any) : (skip(unrefx, value) as T);
